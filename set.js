28081f193c2e076ac4f0c76661de2222ccdfffe8
Ntobeko55555 <nompumelelosibanda82@gmail.com>
2024-12-02 22:30:58 +0200

    Update set.js

--- set.js
+++ set.js
@@ -14,24 +14,24 @@ global.pluginsdb = "https://cmds.giftedtech.my.id"; // DO NOT Change this.... Ch
  
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "", // Add sess Id here espwcially when deploying on panels else use app.json and .env file...
-SUDO_NUMBERS: process.env.SUDO_NUMBERS || "", //Add multiple Numbers with Country Codes without (+) Separated by Comma...
+SUDO_NUMBERS: process.env.SUDO_NUMBERS || "Gifted~q74zHLCZ#taVyCrK2SwGJpmIRBH84ECNCjxtoGL_izno1mcTnVaM", //Add multiple Numbers with Country Codes without (+) Separated by Comma...
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTOLIKE_STATUS: process.env.AUTOLIKE_STATUS || "true",
 AUTOLIKE_EMOJI: process.env.AUTOLIKE_EMOJI || "💜", //Input Yours custom...
 AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
 STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "✅️ Status Viewed by Gifted-Md!", // // Input Yours custom...
 MODE: process.env.MODE || "public",
-OWNER_NUMBER: process.env.OWNER_NUMBER || "", // Only 1 owner Number Here, others Add to sudo numbers...
+OWNER_NUMBER: process.env.OWNER_NUMBER || "27782196277", // Only 1 owner Number Here, others Add to sudo numbers...
 OWNER_NAME: process.env.OWNER_NAME || "𝐆𝐈𝐅𝐓𝐄𝐃 𝐓𝐄𝐂𝐇", // Input Yours custom...(Maintain font for Flow)
-PREFIX: process.env.PREFIX || ".",
-ANTIDELETE: process.env.ANTIDELETE || "false",
-ANTICALL: process.env.ANTICALL || "false",
+PREFIX: process.env.PREFIX || ",",
+ANTIDELETE: process.env.ANTIDELETE || "true",
+ANTICALL: process.env.ANTICALL || "true",
 ANTICALL_MSG: process.env.ANTICALL_MSG || "*_📞 Auto Call Reject Mode Activated by Gifted-Md. 📵 No Calls Allowed Dude!_*",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 BOT_NAME: process.env.BOT_NAME || "𝐆𝐈𝐅𝐓𝐄𝐃-𝐌𝐃", // Input Yours custom...(Maintain font for Flow)
 BOT_PIC: process.env.BOT_PIC || "https://files.giftedtech.my.id/file/gifted-md.jpg", // You can Replace with yours...
-AUTO_AUDIO: process.env.AUTO_AUDIO || "false",
-WELCOME: process.env.WELCOME || "false",
+AUTO_AUDIO: process.env.AUTO_AUDIO || "true",
+WELCOME: process.env.WELCOME || "true",
 GOODBYE: process.env.GOODBYE || "false",    
 AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "false",
 AUTO_BLOCK: process.env.AUTO_BLOCK || "212,79", // Add Multiple Country Codes Separated by Comma...
